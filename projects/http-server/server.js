6ec2b43850d7625cef9608d161b32809538cbb24
ayodele <kadelcode@gmail.com>
2026-04-17 21:37:20 +0100

    fix(http-server): add error handler for server listen (#1)

--- projects/http-server/server.js
+++ projects/http-server/server.js
@@ -5,6 +5,17 @@ const server = http.createServer((req, res) => {
     res.end("Hello, Networking World!\n");
 });
 
+// Error handler
+server.on("error", (err) => {
+    console.error("Server error:", err.message);
+
+    if (err.code == "EADDINUSE") {
+        console.error("Port 8080 is already in use.")
+    }
+
+    process.exit(1); // exit gracefully
+})
+
 server.listen(8080, () => {
     console.log("Server running on http://localhost:8080");
 });
